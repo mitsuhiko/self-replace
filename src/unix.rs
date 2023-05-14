675d200cdae2a28c7e2e00213103727b8f56b60c
Armin Ronacher <armin.ronacher@active-4.com>
2023-05-14 15:40:25 +0200

    Fix unix clippy issues

--- src/unix.rs
+++ src/unix.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 /// On Unix a running executable can be safely deleted.
 pub fn self_delete() -> Result<(), io::Error> {
     let exe = env::current_exe()?.canonicalize()?;
-    fs::remove_file(&exe)?;
+    fs::remove_file(exe)?;
     Ok(())
 }
 
@@ -16,14 +16,14 @@ pub fn self_replace(new_executable: &Path) -> Result<(), io::Error> {
 
     let tmp = tempfile::Builder::new()
         .prefix("._tempexeswap")
-        .tempfile_in(&exe.parent().ok_or_else(|| {
+        .tempfile_in(exe.parent().ok_or_else(|| {
             io::Error::new(
                 io::ErrorKind::Other,
                 "executable has no known parent folder",
             )
         })?)?;
-    fs::copy(&new_executable, tmp.path())?;
-    fs::set_permissions(&tmp.path(), old_permissions)?;
+    fs::copy(new_executable, tmp.path())?;
+    fs::set_permissions(tmp.path(), old_permissions)?;
 
     // if we made it this far, try to persist the temporary file and move it over.
     let (_, path) = tmp.keep()?;
